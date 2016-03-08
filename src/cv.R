a9f41e03d7b0200ff4362495ab2cd32716b2aed3
Miquel Torrens <miquel@ciutadella-129-34.eduroamcc.upf.edu>
2016-03-08 16:18:03 +0100

    Updated validation according to Brownlees

--- src/cv.R
+++ src/cv.R
@@ -21,8 +21,66 @@ series <- get(load(file = file)); cat('Loaded file:', file, '\n')
 target <- series[, 'TARGET']
 
 # Our model
+source(paste(SRCDIR, 'var_selection.R', sep = ''))
 source(paste(SRCDIR, 'unlike.R', sep = ''))
 ################################################################################
+
+################################################################################
+# Variable selection
+library(mombf)
+y <- scale(target)
+x <- scale(series[, -1])
+prior <- zellnerprior(tau = nrow(x))
+
+
+ms.unif <- modelSelection(y = y, x = x, priorCoef = prior,
+                          priorDelta = modelunifprior(), niter = 10 ** 5)
+ms.bbin <- modelSelection(y = y, x = x, priorCoef = prior,
+                          priorDelta = modelbbprior(), niter = 10 ** 5)
+
+pp.unif <- postProb(ms.unif)
+nvars <- sapply(strsplit(as.character(pp.unif[, 1]), split = ','), length)
+pp.unif <- tapply(pp.unif$pp, nvars, sum)
+
+pp.bbin <- postProb(ms.bbin)
+nvars <- sapply(strsplit(as.character(pp.bbin[, 1]), split=','),length)
+pp.bbin <- tapply(pp.bbin$pp,nvars,sum)
+
+#Plot posterior distribution of model size
+plot(names(pp.bbin), pp.bbin, type='l', xlab='Number of variables', ylab='Posterior probability', cex.lab=1.5, ylim=c(0,0.3))
+lines(names(pp.unif), pp.unif, col=2)
+legend('topright',c('Beta-Binomial(1,1)','Uniform'),lty=1,col=1:2,cex=1.5)
+
+#List top 5 models under Uniform & Beta-Binomial prior
+postProb(ms.unif)[1:5, ]
+postProb(ms.bbin)[1:5, ]
+
+
+
+
+
+
+# Least Squares Estimator
+fit.mle <- looCV.mle(y = y, x = x)
+
+# Bayesian Model Averaging
+fit.bma <- looCV.bma(y = y, x = x, type = 'bma', priorCoef = prior,
+                     priorDelta = modelbbprior(alpha.p = 1, beta.p = 1),
+                     niter = 10 ** 4)
+
+# Median Probability Model
+fit.med <- looCV.bma(y = y, x = x, type = 'median', priorCoef = prior,
+                     priorDelta = modelbbprior(alpha.p = 1, beta.p = 1),
+                     niter = 10 ** 4)
+
+# Compare predictions for different methods
+plot(fit.bma$pred, fit.med$pred, xlab = 'Bayesian model averaging',
+     ylab = 'Median probability model')
+abline(0, 1)
+plot(fit.bma$pred, fit.mle$pred, xlab = 'Bayesian model averaging',
+     ylab = 'Least squares')
+abline(0, 1)
+
 
 ################################################################################
 validate <- function(series, p2p) {
@@ -49,6 +107,25 @@ validate <- function(series, p2p) {
 
   # Calculate mean squared error (?)
   preds <- series[to.predict, 'TARGET']
+  mse <- mean((truth - preds) ** 2)
+  return(mse)
+}
+
+################################################################################
+validate <- function(series, p2p) {
+# series (data.frame): data with first column being the feature to predict
+# p2p       (numeric): number of points to predict
+################################################################################
+  # Set up lesioned dataset
+  series <- as.matrix(series)
+  to.predict <- (nrow(series) - p2p + 1):nrow(series)
+  
+  preds <- sapply(to.predict, function(x) {
+    #print(dim(series[1:(x - 1), ])); stop()
+    predictor.unlike(y = series[1:(x - 1), ])
+  })
+
+  truth <- series[to.predict, 'TARGET']
   mse <- mean((truth - preds) ** 2)
   return(mse)
 }
--- src/unlike.R
+++ src/unlike.R
@@ -5,9 +5,34 @@ predictor.unlike <- function(y) {
   y1 <- y[1:(nrow(y) - 1), ]
   y2 <- as.data.frame(t(as.data.frame(y[nrow(y), ])))
 
-  # ARMA (1, 1)
+  # Example ARMA (1, 1)
   arma11 <- arima(y1[, 1], order = c(1, 1, 0), xreg = y1[, -1])
   forecast <- predict(arma11, newdata = y2, newxreg = y2[, -1])
+
+  # Next point prediction
+  pred <- as.numeric(forecast[['pred']])  
+  return(pred)
+}
+
+################################################################################
+predictor.unlike <- function(y) {
+################################################################################
+  z <- rbind.data.frame(as.data.frame(y), rep(NA, ncol(y)))
+  cols <- colnames(z)[colnames(z) != 'TARGET']
+  new.cols <- paste(cols, 'lag1', sep = '_')
+  z[, new.cols] <- NA
+
+  inf.obs <- 1:(nrow(z) - 1)
+  new.obs <- nrow(z)
+  for (i in 1:length(cols)) {
+    z[, new.cols[i]] <- c(NA, z[inf.obs, cols[i]])
+  }
+
+  # Example ARMA (1, 1)
+  arma11 <- arima(z[inf.obs, 1], order = c(1, 1, 0),
+                  xreg = z[inf.obs, new.cols])
+  forecast <- predict(arma11, newdata = z[new.obs, ],
+                      newxreg = z[new.obs, new.cols])
 
   # Next point prediction
   pred <- as.numeric(forecast[['pred']])  
